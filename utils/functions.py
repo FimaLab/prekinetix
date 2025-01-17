4b7dfaecb099f533faf1364a4950822274e01c53
rezvanovpavellaba <128293581+rezvanovpavellaba@users.noreply.github.com>
2025-01-17 12:19:08 +0300

    масштаб лин рег, усы и прочее (поправить рендер)

--- utils/functions.py
+++ utils/functions.py
@@ -18,7 +18,116 @@ from docx.enum.section import WD_ORIENT
 from docx.oxml.ns import nsdecls
 from docx.oxml import parse_xml
 from docx.shared import RGBColor
+import matplotlib.pyplot as plt
 
+
+# Применение настроек осей
+def applying_axis_settings(ax, x_settings, y_settings):
+  if x_settings["min"] < x_settings["max"]:
+      ax.set_xlim(x_settings["min"], x_settings["max"])
+      ax.xaxis.set_major_locator(plt.MultipleLocator(x_settings["major"]))
+      ax.xaxis.set_minor_locator(plt.MultipleLocator(x_settings["minor"]))
+
+  if y_settings["min"] < y_settings["max"]:
+      ax.set_ylim(y_settings["min"], y_settings["max"])
+      ax.yaxis.set_major_locator(plt.MultipleLocator(y_settings["major"]))
+      ax.yaxis.set_minor_locator(plt.MultipleLocator(y_settings["minor"]))
+
+def initializing_checkbox_status_graph_scaling_widgets(graph_id):
+   if f'checkbox_status_graph_scaling_widgets_{graph_id}' not in st.session_state:
+      st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] = False
+
+# Функция для инициализации состояния параметров осей
+def initializing_status_graph_scaling_widgets(graph_id,min_value_X,max_value_X,major_ticks_X,minor_ticks_X,
+                                              min_value_Y,max_value_Y,major_ticks_Y,minor_ticks_Y):
+     #Инизиализация состояния виджетов масштабирования графиков
+      if f"X_graphic_min_value_{graph_id}" not in st.session_state:
+          st.session_state[f"X_graphic_min_value_{graph_id}"] = min_value_X
+      
+      #Инизиализация состояния виджетов масштабирования графиков
+      if f"X_graphic_max_value_{graph_id}" not in st.session_state:
+          st.session_state[f"X_graphic_max_value_{graph_id}"] = max_value_X
+
+      #Инизиализация состояния виджетов масштабирования графиков
+      if f"X_graphic_major_ticks_{graph_id}" not in st.session_state:
+          st.session_state[f"X_graphic_major_ticks_{graph_id}"] = major_ticks_X
+
+      #Инизиализация состояния виджетов масштабирования графиков
+      if f"X_graphic_minor_ticks_{graph_id}" not in st.session_state:
+          st.session_state[f"X_graphic_minor_ticks_{graph_id}"] = minor_ticks_X
+
+      #Инизиализация состояния виджетов масштабирования графиков
+      if f"Y_graphic_min_value_{graph_id}" not in st.session_state:
+          st.session_state[f"Y_graphic_min_value_{graph_id}"] = min_value_Y
+      
+      #Инизиализация состояния виджетов масштабирования графиков
+      if f"Y_graphic_max_value_{graph_id}" not in st.session_state:
+          st.session_state[f"Y_graphic_max_value_{graph_id}"] = max_value_Y
+
+      #Инизиализация состояния виджетов масштабирования графиков
+      if f"Y_graphic_major_ticks_{graph_id}" not in st.session_state:
+          st.session_state[f"Y_graphic_major_ticks_{graph_id}"] = major_ticks_Y
+
+      #Инизиализация состояния виджетов масштабирования графиков
+      if f"Y_graphic_minor_ticks_{graph_id}" not in st.session_state:
+          st.session_state[f"Y_graphic_minor_ticks_{graph_id}"] = minor_ticks_Y
+
+# Функция для настройки осей
+def axis_settings(axis_name,graph_id,min_value,max_value,major_ticks,minor_ticks):
+
+    # Сохранение значений в сессии
+    with st.expander(f"Настройка параметров оси {axis_name} '{graph_id}'"):
+         min_value = st.number_input(f"Граница Минимум ({axis_name})", value=st.session_state[min_value], step=1.0, key=f"{axis_name}_min_{graph_id}")
+         st.session_state[f"{axis_name}_graphic_min_value_{graph_id}"] = min_value
+
+         max_value = st.number_input(f"Граница Максимум ({axis_name})", value=st.session_state[max_value], step=1.0, key=f"{axis_name}_max_{graph_id}")
+         st.session_state[f"{axis_name}_graphic_max_value_{graph_id}"] = max_value
+
+         major_ticks = st.number_input(f"Основная единица измерения ({axis_name})", value=st.session_state[major_ticks], step=0.1, key=f"{axis_name}_major_{graph_id}")
+         st.session_state[f"{axis_name}_graphic_major_ticks_{graph_id}"] = major_ticks
+
+         minor_ticks = st.number_input(f"Дополнительная единица измерения ({axis_name})", value=st.session_state[minor_ticks], step=0.1, key=f"{axis_name}_minor_{graph_id}")
+         st.session_state[f"{axis_name}_graphic_minor_ticks_{graph_id}"] = minor_ticks
+
+
+         # Проверка корректности значений
+         errors = []
+         if min_value >= max_value:
+             errors.append("Минимальное значение должно быть меньше максимального.")
+
+         if major_ticks <= 0:
+             errors.append("Основная единица измерения должна быть больше 0.")
+
+         if minor_ticks <= 0:
+             errors.append("Дополнительная единица измерения должна быть больше 0.")
+
+         if minor_ticks >= major_ticks:
+             errors.append("Дополнительная единица измерения должна быть меньше основной.")
+
+         range_size = max_value - min_value
+         if range_size < major_ticks:
+             errors.append("Основная единица измерения должна быть меньше диапазона оси.")
+
+         if range_size < minor_ticks:
+             errors.append("Дополнительная единица измерения должна быть меньше диапазона оси.")
+
+         if range_size % major_ticks != 0:
+             errors.append("Основная единица измерения не делит диапазон оси без остатка.")
+
+         if range_size % minor_ticks != 0:
+             errors.append("Дополнительная единица измерения не делит диапазон оси без остатка.")
+
+         # Вывод ошибок
+         if errors:
+             for error in errors:
+                 st.warning(error)
+
+         return {
+             "min": min_value,
+             "max": max_value,
+             "major": major_ticks,
+             "minor": minor_ticks,
+         }
 
 # Функция для вычисления критического значения F
 def calculate_f_critical(alpha, df1, df2):
