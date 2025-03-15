4fd29bb1566d7af60c2b24c42ae534dbdb0d5bb8
rezvanovpavellaba <128293581+rezvanovpavellaba@users.noreply.github.com>
2025-03-15 13:31:52 +0300

    Полностью исправил легенду для графиков

--- utils/functions_graphics.py
+++ utils/functions_graphics.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from cycler import cycler
 from matplotlib.ticker import LogLocator, FuncFormatter
+import re
 
 #####Общие функции
 
@@ -100,12 +101,47 @@ def rendering_graphs_with_scale_widgets(graph_id,option,i,kind_graphic,child_fun
 
 
         # Переключатель настройки осей
-        custom_axis = st.checkbox("Настроить параметры осей вручную", value = st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'], key = f"Настроить параметры осей вручную {graph_id}")
+        custom_axis = st.checkbox("Настроить параметры графика вручную", value = st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'], key = f"Настроить параметры графика вручную {graph_id}")
         st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] = custom_axis
 
         new_kwargs = kwargs.copy() if kwargs else {}  # Создаем пустой словарь, если kwargs = None
 
         if st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}']:
+            
+            # Инициализация состояния, если его еще нет
+            if f'legend_x{graph_id}' not in st.session_state:
+                st.session_state[f'legend_x{graph_id}'] = 0.8
+
+            if f'legend_y{graph_id}' not in st.session_state:
+                st.session_state[f'legend_y{graph_id}'] = 0.9
+
+            #if f'fontsize_{graph_id}' not in st.session_state:
+                #st.session_state[f'fontsize_{graph_id}'] = st.session_state[f'default_fontsize_{graph_id}']
+            
+            #if f'markerscale_{graph_id}' not in st.session_state:
+                #st.session_state[f'markerscale_{graph_id}'] = st.session_state[f'default_markerscale_{graph_id}']
+
+            #if f'handlelength_{graph_id}' not in st.session_state:
+                #st.session_state[f'handlelength_{graph_id}'] = st.session_state[f'default_handlelength_{graph_id}']
+
+            #if f'handleheight_{graph_id}' not in st.session_state:
+                #st.session_state[f'handleheight_{graph_id}'] = st.session_state[f'default_handleheight_{graph_id}']
+            
+            with st.expander(f"Настройка легенды"):
+                legend_x = st.slider("X-позиция легенды", 0.0, 1.0, st.session_state[f'legend_x{graph_id}'], key = f'key_legend_x{graph_id}')
+                legend_y = st.slider("Y-позиция легенды", 0.0, 1.0, st.session_state[f'legend_y{graph_id}'], key = f'key_legend_y{graph_id}')
+
+                #fontsize = st.slider("Размер шрифта", 1, 20, st.session_state[f'fontsize_{graph_id}'])
+                #markerscale = st.slider("Размер маркеров", 0.1, 3.0, st.session_state[f'markerscale_{graph_id}'])
+                #handlelength = st.slider("Длина линии", 0.5, 5.0, st.session_state[f'handlelength_{graph_id}'])
+                #handleheight = st.slider("Высота линии", 0.5, 3.0, st.session_state[f'handleheight_{graph_id}'])
+
+                # Обновление session_state при изменении слайдеров
+                #st.session_state[f'fontsize_{graph_id}'] = fontsize
+                #st.session_state[f'markerscale_{graph_id}'] = markerscale
+                #st.session_state[f'handlelength_{graph_id}'] = handlelength
+                #st.session_state[f'handleheight_{graph_id}'] = handleheight
+
             # Настройка осей через виджеты
             x_settings = axis_settings("X",graph_id,f"X_graphic_min_value_{graph_id}",f"X_graphic_max_value_{graph_id}",
                                         f"X_graphic_major_ticks_{graph_id}",f"X_graphic_minor_ticks_{graph_id}")  # Виджет для оси X
@@ -114,12 +150,20 @@ def rendering_graphs_with_scale_widgets(graph_id,option,i,kind_graphic,child_fun
             
             new_kwargs["x_settings"] = x_settings
             new_kwargs["y_settings"] = y_settings
+
+            new_kwargs["legend_x"] = legend_x
+            new_kwargs["legend_y"] = legend_y
+            
+            #new_kwargs["fontsize"] = fontsize
+            #new_kwargs["markerscale"] = markerscale
+            #new_kwargs["handlelength"] = handlelength
+            #new_kwargs["handleheight"] = handleheight
             
             st.session_state[f'x_settings_{graph_id}'] = x_settings
 
             st.session_state[f'y_settings_{graph_id}'] = y_settings
             
-            if st.button("Перерисовать график",key = f'Перерисовать график{graph_id}'):
+            if st.button("Обновить график",key = f'Обновить график{graph_id}'):
                 #вызов функции 
                 fig = child_func_create_graphic(*args, **new_kwargs)         
                 st.session_state[f"list_graphics_word_{option}"][i] = fig
@@ -207,7 +251,7 @@ def create_session_type_graphics_checked_graphics(option,type_graphics):
     if f"{type_graphics}_{option}_checked_graphics" not in st.session_state:
        st.session_state[f"{type_graphics}_{option}_checked_graphics"] = False
     
-    checked_graphics = st.checkbox("Отрисовать графики", value = st.session_state[f"{type_graphics}_{option}_checked_graphics"], key=f"{type_graphics}")
+    checked_graphics = st.checkbox("Показать графики", value = st.session_state[f"{type_graphics}_{option}_checked_graphics"], key=f"{type_graphics}")
     st.session_state[f"{type_graphics}_{option}_checked_graphics"] = checked_graphics
 
 # Применение настроек осей
@@ -323,8 +367,8 @@ def initializing_status_graph_scaling_widgets(graph_id,min_value_X,max_value_X,m
 def axis_settings(axis_name,graph_id,min_value,max_value,major_ticks,minor_ticks):
     
     # Сохранение значений в сессии
-    with st.expander(f"Настройка параметров оси {axis_name} '{graph_id}'"):
-
+    with st.expander(f"Настройка параметров оси {axis_name}"):
+        
          min_value = st.session_state[min_value]
          st.session_state[f"{axis_name}_graphic_min_value_{graph_id}"] = min_value
 
@@ -406,11 +450,16 @@ def checking_file_names_organ_graphs(option,file_name):
     
     return measure_unit_org
 
-def create_individual_graphics(list_time,list_concentration,measure_unit_time, measure_unit_concentration, kind_graphic,graph_id,x_settings=None,y_settings=None):
+def create_individual_graphics(list_time,list_concentration,measure_unit_time, measure_unit_concentration, kind_graphic,graph_id,x_settings=None,y_settings=None,legend_x=None,legend_y=None):
+    
+    # Регулярное выражение для извлечения всего после "№"
+    pattern = r"№\s*(\d+.*)"
+    match = re.search(pattern, graph_id)
+    number_animal = match.group(1)
 
     fig, ax = plt.subplots()
 
-    plt.plot(list_time,list_concentration, marker='o',markersize=4.0,color = "black",markeredgecolor="black",markerfacecolor="black",zorder=10)
+    plt.plot(list_time,list_concentration, marker='o',markersize=4.0,color = "black",markeredgecolor="black",markerfacecolor="black",zorder=10, label=f'{number_animal}')
     # Убираем рамку вокруг графика
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
@@ -430,6 +479,10 @@ def create_individual_graphics(list_time,list_concentration,measure_unit_time, m
         ax.spines['left'].set_color('grey') # Ось Y
     plt.xlabel(f"Время, {measure_unit_time}")
     plt.ylabel("Концентрация, "+measure_unit_concentration)
+
+    # Добавляем легенду с номером животного
+    legend = ax.legend(loc='upper right', frameon=True)  # Можно поменять loc на другой угол
+    legend.set_draggable(True)  # Позволяет перетаскивать легенду
    
     if st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and x_settings is not None:
                 applying_axis_settings(ax, x_settings, y_settings,kind_graphic)
@@ -437,6 +490,10 @@ def create_individual_graphics(list_time,list_concentration,measure_unit_time, m
     #Установка значений из автомат подобранных библиотекой состояния виджетов масштабирования графиков
     else:
         get_parameters_axis(graph_id, ax,list_time)
+
+    if st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and legend_x is not None:
+       # Добавляем легенду с пользовательскими координатами
+       ax.legend(loc=(legend_x, legend_y))
     
     if (st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and x_settings is not None) ==  False:
        ymin, ymax = ax.get_ylim()
@@ -484,7 +541,9 @@ def replace_value_less_one_plot_total_individual_pk_profiles(df_for_plot_conc_1)
     return df_for_plot_conc_1_log
 
 #функция построения графика объединенного индивидуальных профелей
-def plot_total_individual_pk_profiles(list_color,df_for_plot_conc_1,list_numer_animal_for_plot,measure_unit_time,measure_unit_concentration,count_numer_animal,kind_graphic,graph_id,x_settings=None,y_settings=None):
+def plot_total_individual_pk_profiles(list_color,df_for_plot_conc_1,list_numer_animal_for_plot,measure_unit_time,measure_unit_concentration,count_numer_animal,kind_graphic,graph_id,
+                                      x_settings=None,y_settings=None,legend_x=None,legend_y=None):
+   
     fig, ax = plt.subplots()
 
     ax.set_prop_cycle(cycler(color=list_color))
@@ -523,6 +582,35 @@ def plot_total_individual_pk_profiles(list_color,df_for_plot_conc_1,list_numer_a
     #Установка значений из автомат подобранных библиотекой состояния виджетов масштабирования графиков
     else:
         get_parameters_axis(graph_id, ax)
+
+        # Получаем объект легенды
+        #legend = ax.get_legend()
+
+        # Если легенда существует, извлекаем параметры
+        #if legend:
+            #fontsize = legend.get_texts()[0].get_fontsize()  # Размер шрифта
+            #markerscale = legend.legendHandles[0].get_markersize() / plt.rcParams["lines.markersize"]  # Масштаб маркеров
+            #handlelength = legend.legendHandles[0].get_linewidth()  # Длина маркеров (альтернативный способ)
+            # Высота маркеров (нет прямого метода, но можно оценить через pad)
+            #handleheight = legend.legendHandles[0].get_markersize() if legend.legendHandles else None 
+
+            #if f'default_fontsize_{graph_id}' not in st.session_state:
+                #st.session_state[f'default_fontsize_{graph_id}'] = int(fontsize)
+            
+            #if f'default_markerscale_{graph_id}' not in st.session_state:
+                #st.session_state[f'default_markerscale_{graph_id}'] = markerscale
+
+            #if f'default_handlelength_{graph_id}' not in st.session_state:
+                #st.session_state[f'default_handlelength_{graph_id}'] = handlelength
+
+            #if f'default_handleheight_{graph_id}' not in st.session_state:
+                #st.session_state[f'default_handleheight_{graph_id}'] = handleheight
+
+
+    if st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and legend_x is not None:
+       # Добавляем легенду с пользовательскими координатами
+       ax.legend(loc=(legend_x, legend_y))#prop={'size': fontsize},markerscale=markerscale,handlelength=handlelength,handleheight=handleheight)
+       ax.legend(fontsize=(160/count_numer_animal),bbox_to_anchor=(1, 1))
     
     if (st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and x_settings is not None) ==  False:
        ymin, ymax = ax.get_ylim()
@@ -564,12 +652,14 @@ def first_creating_plot_total_individual_pk_profiles(graph_id,list_color,df_for_
         add_or_replace_df_graph(*child_args,fig)
 
 #функция построения графика индивидуального срединных профелей
-def plot_pk_profile_individual_mean_std(list_time,list_concentration,err_y_1,measure_unit_time,measure_unit_concentration,kind_graphic,graph_id,x_settings=None,y_settings=None):
+def plot_pk_profile_individual_mean_std(list_time,list_concentration,err_y_1,measure_unit_time,measure_unit_concentration,kind_graphic,graph_id,file_name,x_settings=None,y_settings=None,legend_x=None,legend_y=None):
+    
+
     fig, ax = plt.subplots()
     if kind_graphic == 'log':
-       plt.plot(list_time,list_concentration, marker='o',markersize=4.0,color = "black",markeredgecolor="black",markerfacecolor="black", zorder=10)
+       plt.plot(list_time,list_concentration, marker='o',markersize=4.0,color = "black",markeredgecolor="black",markerfacecolor="black", zorder=10,label = file_name)
     else:
-       plt.errorbar(list_time,list_concentration,yerr=err_y_1, marker='o',markersize=4.0,color = "black",markeredgecolor="black",markerfacecolor="black",ecolor="black",elinewidth=0.8,capsize=2.0,capthick=1.0,zorder=10)
+       plt.errorbar(list_time,list_concentration,yerr=err_y_1, marker='o',markersize=4.0,color = "black",markeredgecolor="black",markerfacecolor="black",ecolor="black",elinewidth=0.8,capsize=2.0,capthick=1.0,zorder=10,label = file_name)
     
     # Убираем рамку вокруг графика
     ax.spines['top'].set_visible(False)
@@ -592,12 +682,18 @@ def plot_pk_profile_individual_mean_std(list_time,list_concentration,err_y_1,mea
     plt.xlabel(f"Время, {measure_unit_time}")
     plt.ylabel("Концентрация, "+measure_unit_concentration)
 
+    ax.legend()
+
     if st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and x_settings is not None:
                 applying_axis_settings(ax, x_settings, y_settings,kind_graphic)
 
     #Установка значений из автомат подобранных библиотекой состояния виджетов масштабирования графиков
     else:
         get_parameters_axis(graph_id, ax, list_time)
+
+    if st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and legend_x is not None:
+       # Добавляем легенду с пользовательскими координатами
+       ax.legend(loc=(legend_x, legend_y))
     
     if (st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and x_settings is not None) ==  False:
        ymin, ymax = ax.get_ylim()
@@ -618,7 +714,7 @@ def plot_pk_profile_individual_mean_std(list_time,list_concentration,err_y_1,mea
 
     return fig
 
-def first_creating_plot_pk_profile_individual_mean_std(graph_id,list_time,list_concentration,err_y_1,measure_unit_time,measure_unit_concentration,kind_graphic,add_or_replace_df_graph, child_args):
+def first_creating_plot_pk_profile_individual_mean_std(graph_id,list_time,list_concentration,err_y_1,measure_unit_time,measure_unit_concentration,kind_graphic,file_name,add_or_replace_df_graph, child_args):
     #Инициализация состояния чекбокса параметров осей
     initializing_checkbox_status_graph_scaling_widgets(graph_id)
 
@@ -633,7 +729,7 @@ def first_creating_plot_pk_profile_individual_mean_std(graph_id,list_time,list_c
     if st.session_state[f"first_creating_graphic{graph_id}"]:
         #вызов функции построения графика индивидуального срединных профелей линейный
         fig = plot_pk_profile_individual_mean_std(list_time,list_concentration,err_y_1,measure_unit_time,
-                                                            measure_unit_concentration,kind_graphic,graph_id)
+                                                            measure_unit_concentration,kind_graphic,graph_id,file_name)
 
         add_or_replace_df_graph(*child_args,fig)
 
@@ -647,7 +743,7 @@ def replace_value_less_one_plot_pk_profile_total_mean_std_doses_organs(df_concat
     return df_concat_mean_std
 
 ###построение графика "Фармакокинетический профиль в различных органах или дозировках" сравнительные срединные
-def plot_pk_profile_total_mean_std_doses_organs(list_zip_mean_std_colors,list_t,df_concat_mean_std,measure_unit_time,measure_unit_concentration,kind_graphic,graph_id,x_settings=None,y_settings=None):
+def plot_pk_profile_total_mean_std_doses_organs(list_zip_mean_std_colors,list_t,df_concat_mean_std,measure_unit_time,measure_unit_concentration,kind_graphic,graph_id,x_settings=None,y_settings=None,legend_x=None,legend_y=None):
 
     fig, ax = plt.subplots()
 
@@ -686,6 +782,10 @@ def plot_pk_profile_total_mean_std_doses_organs(list_zip_mean_std_colors,list_t,
     else:
         get_parameters_axis(graph_id, ax)
     
+    if st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and legend_x is not None:
+       # Добавляем легенду с пользовательскими координатами
+       ax.legend(loc=(legend_x, legend_y))
+
     if (st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and x_settings is not None) ==  False:
        ymin, ymax = ax.get_ylim()
        padding_x_1 = (max(list_t) - min(list_t)) * 0.01
@@ -738,7 +838,7 @@ def plot_tissue_accessibility(list_name_organs,list_ft):
 
 ###линейная регрессия
 def create_graphic_lin(df_for_lin_mean,measure_unit_dose_lin,measure_unit_lin_concentration,
-                measure_unit_lin_time,graph_id,model,kind_graphic,x_settings=None,y_settings=None):
+                measure_unit_lin_time,graph_id,model,kind_graphic,x_settings=None,y_settings=None,legend_x=None,legend_y=None):
 
     fig, ax = plt.subplots()
 
@@ -784,12 +884,21 @@ def create_graphic_lin(df_for_lin_mean,measure_unit_dose_lin,measure_unit_lin_co
     else:
         get_parameters_axis(graph_id, ax)
 
+    
     # Определяем положение аннотации динамически
     max_y = df_for_lin_mean['AUC0→∞_mean'].max() + df_for_lin_mean['AUC0→∞_std'].max()  # Максимальное значение Y с учетом ошибок
     x_pos = df_for_lin_mean['doses'].mean()  # Среднее значение доз для X
     y_pos = max_y * 1.05  # Смещаем немного выше максимального значения
+    
+    if st.session_state[f'checkbox_status_graph_scaling_widgets_{graph_id}'] and legend_x is not None:
+       # Получаем реальные границы графика
+       x_min, x_max = plt.xlim()
+       y_min, y_max = plt.ylim()
+       # Добавляем легенду с пользовательскими координатами
+       x_pos = x_min + legend_x * (x_max - x_min)  # Преобразуем относительное значение в реальные координаты X
+       y_pos = y_min + legend_y * (y_max - y_min)  # Преобразуем относительное значение в реальные координаты Y
 
-
+    
     plt.annotate(
         'y = {:.2f}x {} {:.2f}\n$R^2$ = {:.3f}'.format(
             round(model.params[1], 2),  # Коэффициент при x
